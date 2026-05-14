a376ee5adf4e32d187dec5302e84991ea0d54421
gkwgu <polina.petr0v4@yandex.ru>
2026-05-14 10:45:01 +0300

    fix

--- src/main/java/com/inventory/inventorySystem/config/CorsConfig.java
+++ src/main/java/com/inventory/inventorySystem/config/CorsConfig.java
@@ -14,10 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "https://inventory-client-lime.vercel.app"
-                        )
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
